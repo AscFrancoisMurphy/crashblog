031f3b3eef323788b40b6991ff486536cf250db3
Simon boulou <you@example.com>
2018-02-27 14:11:26 +0100

    view create autor title category

---
+++ Article.php
@@ -0,0 +1,36 @@
+
+    <div class="cont_article">
+
+<p>Titre</p>
+  <input type="text" value="">
+       
+</select>
+
+     <p>Auteur</p>
+<select id="Auteur"> 
+  <option value="Auteur 1">Valeur 1</option> 
+  <option value="Auteur 2">Valeur 2</option>
+  <option value="Auteur 3">Valeur 3</option>
+  <input type="submit" value="Créer">
+</select>
+ <p>Catégorie</p>
+<select id="Categorie"> 
+  <option value="Categorie 1">Valeur 1</option> 
+  <option value="Categorie 2">Valeur 2</option>
+  <option value="Categorie 3">Valeur 3</option>
+  <input type="submit" value="Créer">
+</select>
+
+
+        <div class="aff_categorie">
+
+           
+           <textarea name="textarea"
+            rows="50" cols="200">
+  Vous pouvez écrire votre article ici
+        </textarea>
+        <input type="submit" value="Valider">
+        </div>
+        <a href="">x</a>
+</div>
+
