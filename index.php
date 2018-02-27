244ed24a07227bd964e3b3438c98522cabd1956e
wardmar <alexis.wardmar@gmail.com>
2018-02-27 16:03:30 +0100

    megusta

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <form action="index.php">
                 <select name="" id="">
                     <option value="article">Article</option>
-                    <option value="autor">Autor</option>
+                    <option value="auteur">Autor</option>
                     <option value="categorie">Catégorie</option>
                 </select>
                 <input type="button" value="créer">
@@ -23,7 +23,7 @@
         <div>
             <form action="index.php">
                 <select name="" id="">
-                    <option value="autor">Autor</option>
+                    <option value="auteur">Autor</option>
                 </select>
                 <input type="text" name="" id="" value="nom de l'article">
                 <input type="button" value="Date">
@@ -36,9 +36,39 @@
     </div>
    
     <table>
+        <?php
 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=CrashBlog_Equipe2;charset=utf8', 'Dev', 'luwasx18500');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+    $reponse = $bdd->query('SELECT * FROM Article');
+
+    while ($donnees = $reponse->fetch())
+{
+?>
+    <tr>
+    <td><?php echo $donnees['Art_titre']; ?></td>
+    <td><?php echo $donnees['Cate_nom']; ?></td>
+    <td><?php echo $donnees['Art_aut']; ?></td>
+    <td><?php echo $donnees['Art_date']; ?></td>
+    </tr>
+<?php
+}
+
+$reponse->closeCursor();
+
+?>
+    
     </table>
-    <div class="popup auteur">
+    <?php
+    include 'categorie.php';
+    ?>
+     <div class="popup auteur">
     <?php
         include 'php/Auteurs.php';
     ?>
