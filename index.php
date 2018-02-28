5eb06ebc006f9fd27f1189555bdaec98969c7056
wardmar <alexis.wardmar@gmail.com>
2018-02-28 15:49:37 +0100

    itsatrap

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -9,9 +9,19 @@
 </head>
 
 <body>
+    <?php
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=CrashBlog_Equipe2;charset=utf8', 'Dev', 'luwasx18500',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+    ?>
     <div>
         <div>
-            <form action="index.php">
+            <form action="index.php" method="post">
                 <select name="" id="">
                     <option value="article">Article</option>
                     <option value="auteur">Autor</option>
@@ -21,33 +31,74 @@
             </form>
         </div>
         <div>
-            <form action="index.php">
-                <select name="" id="">
-                    <option value="auteur">Autor</option>
+            <form action="index.php" method="post">
+                <select name="auteurs" id="">
+                    <option value="autor">Autor</option>
+                    <?php
+
+
+                            $aut = $bdd->query('SELECT * FROM Auteur');
+
+                            while ($donnees = $aut->fetch())
+                        {
+                        ?>
+                            <tr>
+                            <option value="<?php echo($donnees['auto']) ?>"><?php echo($donnees['auto']) ?></option>
+                            </tr>
+                        <?php
+                        }
+
+                        $aut->closeCursor();
+
+                        ?>
                 </select>
-                <input type="text" name="" id="" value="nom de l'article">
-                <input type="button" value="Date">
-                <select name="" id="">
+                <select name="categ" id="">
                     <option value="categorie">Catégorie</option>
+                    <?php
+
+                            $cato = $bdd->query('SELECT * FROM Categorie');
+
+                            while ($donnees = $cato->fetch())
+                        {
+                        ?>
+                            <tr>
+                            <option value="<?php echo($donnees['Cat']) ?>"><?php echo($donnees['Cat']) ?></option>
+                            </tr>
+                        <?php
+                        }
+
+                        $cato->closeCursor();
+
+                        ?>
                 </select>
-                <input type="button" value="Trier par">
+                <input type="submit" value="Trier par" name="sub">
             </form>
         </div>
     </div>
-   
-    <table>
-        <?php
-
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=CrashBlog_Equipe2;charset=utf8', 'Dev', 'luwasx18500');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+    
+     <table>
+    <?php
+    
     $reponse = $bdd->query('SELECT * FROM Article');
-
+        if (isset($_POST["sub"])){
+           
+            if($_POST["auteurs"]!==autor){
+                $done = $_POST["auteurs"];
+                $reponse = $bdd->prepare('SELECT * FROM Article WHERE Art_aut = ? ');
+                $reponse->execute(array($done));  
+            }
+           
+            elseif($_POST["categ"]!==categorie){
+                $dcate = $_POST["categ"];
+                $reponse = $bdd->prepare('SELECT * FROM Article WHERE Cate_nom = ? ');
+                $reponse->execute(array($dcate));  
+            } else{
+               $reponse = $bdd->query('SELECT * FROM Article'); 
+            } 
+            }  
+        ?>
+        <?php
+        
     while ($donnees = $reponse->fetch())
 {
 ?>
